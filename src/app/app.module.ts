c192a21b75e8d5951143d277c0112ebd86fac142
Eduardo <eduundav@gmail.com>
2022-04-02 15:46:36 -0300

    modularizacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { PresentacionComponent } from './componentes/presentacion/presentacion.component';
+import { MisInteresesComponent } from './componentes/mis-intereses/mis-intereses.component';
+import { DisponibilidadComponent } from './componentes/disponibilidad/disponibilidad.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { SkillsComponent } from './componentes/skills/skills.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { ContactosComponent } from './componentes/contactos/contactos.component';
+import { FooterComponent } from './componentes/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PresentacionComponent,
+    MisInteresesComponent,
+    DisponibilidadComponent,
+    EducacionComponent,
+    SkillsComponent,
+    ProyectosComponent,
+    ContactosComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/componentes/mis-intereses/mis-intereses.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MisInteresesComponent } from './mis-intereses.component';
+
+describe('MisInteresesComponent', () => {
+  let component: MisInteresesComponent;
+  let fixture: ComponentFixture<MisInteresesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ MisInteresesComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MisInteresesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
